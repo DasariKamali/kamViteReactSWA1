14950ea396f89ae342ea8f3a5a7b0271854764c3
DasariKamali <107987424+DasariKamali@users.noreply.github.com>
2025-03-03 16:40:35 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Base API client for React App 1
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://kamspringweb-chc7arb5atc2cmg9.canadacentral-01.azurewebsites.net/api',
   headers: {
     'Content-Type': 'application/json'
   }
